55bf7b579538446077784e655fc1faba12f297c8
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-02-21 13:24:48 -0500

    registered production model

--- rea_project/rea_app/admin.py
+++ rea_project/rea_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Season
+from .models import Season, Production
 
 # Register your models here.
 admin.site.register(Season)
+admin.site.register(Production)
