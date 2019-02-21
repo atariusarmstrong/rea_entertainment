852fb835ec19c0de16e074dccf26f1234b7e06d0
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-02-21 14:14:21 -0500

    tried updating models

--- rea_project/rea_app/models.py
+++ rea_project/rea_app/models.py
@@ -1,24 +1,19 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Season(models.Model):
-    name: models.CharField(max_length=60)
-    year: models.IntegerField(
-        validators=[
-            MinValueValidator(4),
-            MaxValueValidator(4)
-        ])
+    name = models.CharField(max_length=60)
+    year = models.IntegerField()
     
     def __str__(self):
         return self.name
 
 class Production(models.Model):
-    title: models.CharField(max_length=120)
-    start_date: models.DateField()
-    end_date: models.DateField()
-    description: models.TextField()
     season = models.ForeignKey(Season, on_delete=models.CASCADE, related_name="productions")
-
+    title = models.CharField(max_length=120)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+    
     def __str__(self):
         return self.title
