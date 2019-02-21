701aa39e75c98bc83076a3448d429628ab760f32
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-02-21 13:11:08 -0500

    added first model - season

--- rea_project/rea_app/models.py
+++ rea_project/rea_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Season(models.Model):
+    name: models.CharField(max_length=60)
+    year: models.IntegerField(
+        validators=[
+            MinValueValidator(4),
+            MaxValueValidator(4)
+        ])
+    
+    def __str__(self):
+        return self.name
