b35fd3d74b421f0ae82b731fc629a4e579d28318
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-02-25 17:22:47 -0500

    added navbar to productions

--- client/src/components/Productions/ProductionIndex.js
+++ client/src/components/Productions/ProductionIndex.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from '../Navbar';
 
 class ProductionIndex extends Component {
     render() {
         return (
             <div>
+                <Navbar />
                 Here is a a list of productions
             </div>
         );
