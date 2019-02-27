6107285f2ee83c79583695828a01cc2cc8a0c467
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-02-27 10:38:40 -0500

    added navbar to education index

--- client/src/components/Education/EducationProgramIndex.js
+++ client/src/components/Education/EducationProgramIndex.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import Navbar from '../Navbar';
 
 class EducationProgramIndex extends Component {
     state = {
@@ -13,10 +14,13 @@ class EducationProgramIndex extends Component {
     render() {
         return (
             <div>
+                <Navbar/>
                 {this.state.educationalprograms.map((program, i) => {
                     return (
                         <div key={i}>
-                            <h2>{program.title}</h2>
+                            <h2>{program.title}</h2><h4>with {program.instructor}</h4>
+                            <p>{program.description}</p>
+                            <p>Price: ${program.price}</p>
                         </div>
                     )
                 })}
