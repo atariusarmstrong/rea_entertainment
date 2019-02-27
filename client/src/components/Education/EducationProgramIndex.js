cd40b06cf79dac1aed10657b7b3a29efc4b6bd21
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-02-27 10:49:53 -0500

    styled educational index

--- client/src/components/Education/EducationProgramIndex.js
+++ client/src/components/Education/EducationProgramIndex.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 import Navbar from '../Navbar';
+import styled from 'styled-components'
 
+const EducationBox = styled.div`
+    color: #707070;
+    background-color: white;
+    height: 163px;
+    width: 859px;
+    margin: 0 auto;
+`
+
+const Header = styled.div`
+    width: 640px;
+    height: 68px;
+    color: white;
+    background-color: #A22929;
+    text-transform: uppercase;
+    h1 {
+        margin-left: 20px;
+    }
+`
 class EducationProgramIndex extends Component {
     state = {
         educationalprograms: [{}]
@@ -15,13 +34,18 @@ class EducationProgramIndex extends Component {
         return (
             <div>
                 <Navbar/>
+                <Header>
+                    <h1>Educational Programs</h1>
+                </Header>
+                
+
                 {this.state.educationalprograms.map((program, i) => {
                     return (
-                        <div key={i}>
+                        <EducationBox key={i}>
                             <h2>{program.title}</h2><h4>with {program.instructor}</h4>
                             <p>{program.description}</p>
                             <p>Price: ${program.price}</p>
-                        </div>
+                        </EducationBox>
                     )
                 })}
             </div>
