a7e626f7c2f7f62e14638c9af7bd044cdf8d9c16
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-02-27 22:16:03 -0500

    checked out previous commit

---
+++ client/src/GoogleCal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GoogleCal extends Component {
+    render() {
+        return (
+            <div>
+                Testing google calendar
+            </div>
+        );
+    }
+}
+
+export default GoogleCal;
--- client/src/components/Productions/ProductionIndex.js
+++ client/src/components/Productions/ProductionIndex.js
@@ -6,8 +6,21 @@ import styled from 'styled-components'
 
 const ShowBox = styled.div`
     box-shadow: 5px 5px #A22929;
-    background-color: white;
-    color: #707070;
+    background-color: #707070;
+    width: 447.9px;
+    height: 455.8px;
+    img {
+        width: 447.9px;
+        height: 224.93px;
+    }
+    a {
+        text-decoration: none;
+        color: white;
+    }
+`
+const ShowContainer = styled.div`
+    display: grid;
+    grid-template-columns: 50% 50%;
 `
 
 class ProductionIndex extends Component {
@@ -22,22 +35,26 @@ class ProductionIndex extends Component {
         return (
             <div>
                 <Navbar />
-                {this.state.productionList.map((production, i) => {
-                    return (
-                        <ShowBox key={i}>
-                            <Link to={`/season/${production.id}`}>
-                                <div>
-                                    <img src={production.photo_url} alt={production.title}/>
-                                    <h3>{production.title}</h3>
-                                </div>
-                                <div>
-                                    <p>{production.description}</p>
-                                </div>
-                            </Link>
-                            
-                        </ShowBox>
-                    )
-                })}
+                <ShowContainer>
+                    {this.state.productionList.map((production, i) => {
+                        return (
+                            <ShowBox key={i}>
+                                <Link to={`/season/${production.id}`}>
+                                    <div>
+                                        <img src={production.photo_url} alt={production.title}/>
+                                        <h3>{production.title}</h3>
+                                    </div>
+                                    <div>
+                                        <p>{production.description}</p>
+                                    </div>
+                                </Link>
+                                
+                            </ShowBox>
+                        )
+                    })}
+
+                </ShowContainer>
+
             </div>
         );
     }
